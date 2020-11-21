e2b81710167039a70f90a038e896839e0aa9cca6
Benedict <benedict_zeng@live.com.au>
2020-11-22 10:06:13 +1100

    Create VisualStudio files

---
+++ TravelRestrictions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TravelRestrictions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
